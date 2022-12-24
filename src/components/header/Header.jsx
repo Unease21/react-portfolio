51d48789795710edba37b033a7e743879bf6052f
Anish Shrestha <anisheyyy@gmail.com>
2022-12-24 20:20:41 +0545

    day two

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'am</h5>
+        <h1>Anish Shrestha</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+      </div>
+    </header>
   )
 }
 
